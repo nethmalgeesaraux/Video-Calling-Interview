525ea5a997fab966be06d406949a09c3f7f1b91c
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-03-07 13:57:30 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import { Show, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>WellCome</h1>
+    <div className="m-10">
+      {/* <Show when="signed-out">
+        <SignInButton />
+      </Show>
+
+      <Show when="signed-in">
+        <UserButton />
+      </Show> */}
     </div>
   );
 }
