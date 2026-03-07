a732b6b35059a32679711b8c7b0d8a2e4942adb8
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-03-07 13:59:07 +0530

    Sign up & Sign out

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import { Show, SignInButton, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="m-10">
-      {/* <Show when="signed-out">
+      <Show when="signed-out">
         <SignInButton />
       </Show>
 
       <Show when="signed-in">
         <UserButton />
-      </Show> */}
+      </Show>
     </div>
   );
 }
